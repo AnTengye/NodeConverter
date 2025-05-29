981ea9d02926e34b04c8b532dda612acc94897e6
aty <aty@qq.com>
2025-05-29 18:36:15 +0800

    优化trojan的转换

--- core/trojan_test.go
+++ core/trojan_test.go
@@ -26,19 +26,21 @@ func TestTrojanNode_FromShare(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		{
+			name: "trojan-success",
+			args: args{
+				s: "trojan://BIRCauz5R2S8O3pZ56eg8yC43yla4T3D7znxCDFxSCODO9jA3NxAcaE3YYK3Sq8u9DxZg@closet.homeofbrave.net:18332?sni=closet.homeofbrave.net&fp=chrome#%F0%9F%87%BA%F0%9F%87%B8%E7%BE%8E%E5%9B%BD20-%20%E2%AC%87%EF%B8%8F%202.2MB%2Fs",
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			node := &TrojanNode{
-				Normal:        tt.fields.Normal,
-				TLSConfig:     tt.fields.TLSConfig,
-				NetworkConfig: tt.fields.NetworkConfig,
-				Password:      tt.fields.Password,
-				SSOpts:        tt.fields.SSOpts,
-			}
+			node := NewTrojanNode()
 			if err := node.FromShare(tt.args.s); (err != nil) != tt.wantErr {
 				t.Errorf("FromShare() error = %v, wantErr %v", err, tt.wantErr)
 			}
+			t.Log(node.ToClash())
 		})
 	}
 }
